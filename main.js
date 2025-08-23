b9e6c3093e027af988c242550d06ebbe077f3f9f
simonthuandev <nguyenducthuank588@gmail.com>
2025-08-23 23:27:56 +0700

    initial project

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// function automaticClockRender() {
+//   const time = new Date();
+//   const hour = String(time.getHours()).padStart(2, "0");
+//   const minute = String(time.getMinutes()).padStart(2, "0");
+//   const second = String(time.getSeconds()).padStart(2, "0");
+//   const timeString = `${hour}:${minute}:${second}`;
+//   document.getElementById("clock").innerHTML = timeString;
+// }
+// setInterval(automaticClockRender, 1000);
