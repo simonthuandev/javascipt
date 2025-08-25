4413b59e8a911dc9f23008221d29edc732cb3fe3
simonthuandev <nguyenducthuank588@gmail.com>
2025-08-25 16:13:44 +0700

    miniProject - slide Image

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // function automaticClockRender() {
 //   const time = new Date();
 //   const hour = String(time.getHours()).padStart(2, "0");
@@ -8,3 +7,33 @@
 //   document.getElementById("clock").innerHTML = timeString;
 // }
 // setInterval(automaticClockRender, 1000);
+
+// const changeColorButton = document.getElementById("btn");
+// const title = document.getElementById("title");
+// function randomColor() {
+//   let r = Math.floor(Math.random() * 256),
+//     g = Math.floor(Math.random() * 256),
+//     b = Math.floor(Math.random() * 256);
+//   let colorString = `rgb(${r}, ${g}, ${b})`;
+//   changeColorButton.parentElement.style.background = colorString;
+//   title.innerText = colorString;
+// }
+// changeColorButton.addEventListener("click", randomColor);
+// let container = document.querySelector(".container");
+// container.addEventListener("click", function (e) {
+//   if (e.target !== container) {
+//     let currentActive = document.querySelector(".panel.active");
+//     currentActive.classList.remove("active");
+//     e.target.classList.add("active");
+//   }
+// });
+let panelList = document.querySelectorAll(".panel");
+
+panelList.forEach((panel) => {
+  panel.addEventListener("click", function(e) {
+    let currentActive = document.querySelector(".panel.active");
+    currentActive.classList.remove("active");
+    panel.classList.add("active");
+    console.log(e.target);
+  });
+});
