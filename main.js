63953171fc66a30709100769a548992103cbad87
simonthuandev <nguyenducthuank588@gmail.com>
2025-08-29 01:48:57 +0700

    miniProject04 - Button effect

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const input_search = document.querySelector(".input_search");
-const search_icon = document.querySelector(".icon");
-const search_box = document.querySelector(".search-box");
-search_icon.addEventListener("click", function () {
-  search_box.classList.toggle("active");
-  if (search_box.classList.contains("active")) {
-    input_search.focus();
-  }
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function(e) {
+    // lay toa do cua button so voi viewport khi click
+    const buttonRect = e.target.getBoundingClientRect();
+    // lay toa do chuot tai vi tri click so voi viewport
+    const x = e.clientX;
+    const y = e.clientY;
+    // tao hinh tron va tinh toan vi tri dat position
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    this.appendChild(circle);
+    circle.style.left = `${x - buttonRect.left}px`;
+    circle.style.top = `${y - buttonRect.top}px`;
+    // xoa hinh tron sau khi thuc hien xong animation
+    setTimeout(() => { circle.remove(); }, 500);
+  })
 });
